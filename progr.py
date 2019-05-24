6232844aff70c6b089f07272ad454509789c83ee
Andrii Kostiv <kostiv@gmx.net>
2019-05-24 17:04:07 +0300

    add param.yaml

---
+++ progr.py
@@ -0,0 +1,32 @@
+import json
+import yaml
+
+sample = {
+  "Environment": "STAGE",
+  "KeyName": "aws-test-oregon",
+  "NATGWInstanceTypeParameter": "t2.micro",
+  "VPCBlock": "10.0.0.0/16",
+  "PublicSubnetCIDR": "10.0.10.0/24",
+  "PrivateSubnetCIDR": "10.0.11.0/24"
+}
+
+json_obj = json.dumps(sample)
+print('json_obj =', json_obj)
+
+ff = open('params.yaml', 'wb')
+
+stream = open('parammm.yaml', 'r')
+datamap = yaml.safe_load(stream)
+print('json_obj =', datamap)
+output=open('parammm.json', 'w')
+json.dump(datamap, output)
+output.close
+stream.close
+
+# https://stackoverflow.com/questions/51914505/python-yaml-to-json-to-yaml
+yaml.dump(sample, ff, default_flow_style=False)
+type(ff)
+
+ydump = yaml.dump(sample, default_flow_style=False)
+print 'ydump=',ydump
+

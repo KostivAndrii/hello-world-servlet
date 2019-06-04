457da60adb136bc57e6c4f6f387443fa0177982c
Andrii Kostiv <kostiv@gmx.net>
2019-06-04 20:12:55 +0300

    m

--- params2.py
+++ params2.py
@@ -3,6 +3,7 @@ import sys
 import os
 import json
 import yaml
+import argparse
 
 # import boto3
 import boto.cloudformation
@@ -16,6 +17,7 @@ import boto.cloudformation
 # ec2 = boto.ec2.connect_to_region('eu-west-3')
 
 allowed_env = ['DEV','QA','TEST']
+allowed_action = ['CREATE', 'UPDATE', 'TEST', 'BOTO']
 
 def read_cfg(inputfile):
     try:
@@ -39,11 +41,13 @@ def write_json(outputfile, data, KeyName, ValueName):
         print("can''t open destiantion file %s " % inputfile)
         sys.exit(2)
 
-    OutputParam = []
-    for paramm in data:
-        value = data[paramm]
-        Key = {KeyName: paramm, ValueName: value}
-        OutputParam.append(Key)
+    OutputParam = [ {KeyName: paramm, ValueName: data[paramm]} for paramm in data ]
+    # OutputParam = []
+    # for paramm in data:
+    #     # value = data[paramm]
+    #     Key = {KeyName: paramm, ValueName: data[paramm]}
+    #     OutputParam.append(Key)
+    # print('OutputParam2 =', OutputParam2)
     print('OutputParam =', OutputParam)
 
     # path = os.getcwd()
@@ -59,50 +63,79 @@ def run(cmd):
 
 
 def create_cf_boto(cf_stack, cf_template_url, parameters, tags):
-    cf_param = []
-    for paramm in parameters:
-        Key = (paramm, parameters[paramm])
-        cf_param.append(Key)
-    cf_tags = {}
-    for paramm in tags:
-        cf_tags[paramm] = tags[paramm]
-        # cf_tags.append(Key)
+    # cf_param = []
+    # for paramm in parameters:
+    #     Key = (paramm, parameters[paramm])
+    #     cf_param.append(Key)
+    cf_param = [ (paramm, parameters[paramm]) for paramm in parameters ]
+    # cf_tags = {}
+    # for paramm in tags:
+    #     cf_tags[paramm] = tags[paramm]
+    #     # cf_tags.append(Key)
+    cf_tags = { paramm: tags[paramm] for paramm in tags }
     conn = boto.cloudformation.connection.CloudFormationConnection()
     stdout = conn.create_stack(cf_stack, template_body=None, template_url=cf_template_url, parameters=cf_param, notification_arns=[], disable_rollback=False, timeout_in_minutes=None, capabilities=None, tags=cf_tags)
-    return stdout 
+    return stdout
 
 def main():
-    args_count = len(sys.argv[1:])
-    if args_count < 1 :
-        sys.exit('should be described ENVIRONMENT')
-    envir = sys.argv[1]
-    if envir not in allowed_env:
+    parser = argparse.ArgumentParser(description='Programm to work with AWS')
+    parser.add_argument("-e","--env", help="Environment name", type=str)
+    parser.add_argument("-s","--stack", help="STACK name", type=str)
+    parser.add_argument('-a','--action', help='what to do CREATE/UPDATE/BOTO')
+    parser.add_argument('-i','--input', help='file with parameters and tags')
+    parser.add_argument('-cf','--cloud-formation', help='file with parameters and tags')
+    # parser.add_argument('-e','--env', help='Environment', required=True)
+    # parser.add_argument('-s','--stack', help='STACK name', required=True)
+    # parser.add_argument('-a','--action', help='what to do CREATE/UPDATE/BOTO', required=True)
+    # parser.add_argument('-i','--input', help='file with parameters and tags', required=False)
+    args = parser.parse_args()
+    # args = vars(parser.parse_args())
+    if args.env not in allowed_env:
         print('wrong env - we process only', allowed_env)
         sys.exit()
-    if args_count > 1 :
-        inputfile = sys.argv[2]
-    else:
-        inputfile = 'my_cfg.yaml'
-    if args_count > 2 :
-        stack = sys.argv[3]
-    else:
-        stack = 'AWS-NATGW'
-    if args_count > 3 :
-        action = sys.argv[4]
-    else:
-        action = 'CREATE'
+    if args.action not in allowed_action:
+        print('wrong env - we process only', allowed_action)
+        sys.exit()
+    # stack = args['stack']
+    # inputfile = args['input']
 
-    print("script will convert %s into parameters.json and tags.json for ENVIRONMENT %s and %s STACK %s" % (inputfile, envir, action, stack) )
+    # args_count = len(sys.argv[1:])
+    # if args_count < 1 :
+    #     sys.exit('should be described ENVIRONMENT')
+    # envir = sys.argv[1]
+    # if envir not in allowed_env:
+    #     print('wrong env - we process only', allowed_env)
+    #     sys.exit()
+    # if args_count > 1 :
+    #     inputfile = sys.argv[2]
+    # else:
+    #     inputfile = 'my_cfg.yaml'
+    # if args_count > 2 :
+    #     stack = sys.argv[3]
+    # else:
+    #     stack = 'AWS-NATGW'
+    # if args_count > 3 :
+    #     action = sys.argv[4]
+    # else:
+    #     action = 'CREATE'
+
+    # print("script will convert %s into parameters.json and tags.json for ENVIRONMENT %s and %s STACK %s" % (inputfile, envir, action, stack) )
 
     # inputfile = 'params-'+sys.argv[1]+'.yaml'
-    cfg = read_cfg(inputfile)
+    cfg = read_cfg(args.input)
     print('cfg = ', cfg)
 
-    parameters = cfg[0]["parameters"]
-    tags = cfg[1]["tags"]
+    parameters = cfg["parameters"]
+    tags = cfg["tags"]
     print('parameters = ', parameters)
     print('tags = ', tags)
-    
+
+    # # dict comprehensive
+    # team1 = {"Jones": 24, "Jameson": 18, "Smith": 58, "Burns": 7}
+    # team2 = {"White": 12, "Macke": 88, "Perce": 4}
+    # newTeam = {k:v for team in (team1, team) for k,v in team.items()}
+
+
     # cf_param = []
     # for paramm in parameters:
     #     Key = (paramm, parameters[paramm])
@@ -127,17 +160,18 @@ def main():
     stdout = run(cmd)
     print('stdout = ', stdout)
 
-    if action == "CREATE":
-        cmd = "aws cloudformation create-stack --stack-name "+stack+" --template-body file://ec2.yaml --parameters file://parameters.json --tags file://tags.json"
+    if args.action == "CREATE":
+        cmd = "aws cloudformation create-stack --stack-name " + args.stack + \
+              " --template-body file://ec2.yaml --parameters file://parameters.json --tags file://tags.json"
         stdout = run(cmd)
         print('stdout = ', stdout)
-    if action == "UPDATE":
-        cmd = "aws cloudformation update-stack --stack-name "+stack+" --template-body file://ec2.yaml --parameters file://parameters.json --tags file://tags.json"
+    if args.action == "UPDATE":
+        cmd = "aws cloudformation update-stack --stack-name "+args.stack+" --template-body file://ec2.yaml --parameters file://parameters.json --tags file://tags.json"
         stdout = run(cmd)
         print('stdout = ', stdout)
-    if action == "BOTO":
+    if args.action == "BOTO":
         template_url = 'https://s3-external-1.amazonaws.com/cf-templates-1ldvye973texh-us-east-1/20191539Ae-cf-natgw-tomcatuumsynh895'
-        stdout = create_cf_boto(stack, template_url, parameters, tags)
+        stdout = create_cf_boto(args.stack, template_url, parameters, tags)
         print('stdout = ', stdout)
 
     # print('')

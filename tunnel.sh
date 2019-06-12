10daef37f2fdee5de2dc2b6ed595df6c055bfcb1
KostivAndrii <40714680+KostivAndrii@users.noreply.github.com>
2019-06-12 12:54:50 +0300

    Update tunnel.sh

--- tunnel.sh
+++ tunnel.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
 
-ssh -o "StrictHostKeyChecking no" -f -N -L 12345:10.200.11.18:22 ec2-user@35.180.113.227
-ssh -o "StrictHostKeyChecking no" -f -N -L 12345:10.200.11.222:22 ec2-user@35.180.25.127
-ssh -o "StrictHostKeyChecking no" -f -N -L 12345:10.200.11.43:22 ec2-user@35.180.202.191
-ssh -o "StrictHostKeyChecking no" -f -N -L 12345:10.200.11.43:22 ec2-user@35.180.202.191
-ssh -o "StrictHostKeyChecking no" -f -N -L 12345:10.200.11.217:22 ec2-user@35.180.88.110
-ssh -o "StrictHostKeyChecking no" -f -N -L 12345:10.200.11.208:22 ec2-user@35.180.85.186
-ssh -o "StrictHostKeyChecking no" -f -N -L 12345:10.200.11.249:22 ec2-user@52.47.128.228
-ssh -o "StrictHostKeyChecking no" -f -N -L 12345:10.200.11.103:22 ec2-user@35.180.159.10

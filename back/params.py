c6eac1eb6343cc868ccc0742357e0e21443b27a2
Andrii Kostiv <kostiv@gmx.net>
2019-06-27 17:50:11 +0300

    add modules

--- back/params.py
+++ back/params.py
@@ -1,37 +1,89 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import sys
+#import os
 import json
 import yaml
 
-program_name = sys.argv[0]
-envir = sys.argv[1]
-#count = len(sys.argv[1:])
+allowed_env = ['DEV','QA','TEST']
 
-print('the script has the name %s' % program_name)
-#print("the script is called with %i arguments" % count) 
-print("the script will convert params-%s.yaml and tags-%s.yaml into params.json and tags.json" % (sys.argv[1], sys.argv[1]))
-print('we will prepare params.json and tags.json for ENVIRONMENT %s' % envir)
+def process_yaml(inputfile, outputfile):
+  try:
+      stream = open(inputfile, 'r')
+  except FileNotFoundError:
+#      path = os.getcwd()
+#      print("can''t open source file %s\%s " % (path, inputfile))
+      print("can''t open source file %s" % inputfile)
+      sys.exit(1)
+#   try:
+#       output=open(outputfile, 'w')
+#   except FileNotFoundError:
+#       print("can''t open destiantion file %s " % inputfile)
+#       sys.exit(2)
+  datamap = yaml.safe_load(stream)
+  print('json_obj =', datamap)
+
+  Parameters = []
+  for paramm in datamap[0]["Parameters"]:
+      value = datamap[0]["Parameters"][paramm]
+      Key = {"ParameterKey": paramm, "ParameterValue": value}
+      Parameters.append(Key)
+  print('Parameters =', Parameters)
+  try:
+      output=open(outputfile, 'w')
+  except FileNotFoundError:
+      print("can''t open destiantion file %s " % inputfile)
+      sys.exit(2)
+  json.dump(Parameters, output)
+  output.flush()
+  output.close()
+
+  Tags = []
+  for paramm in datamap[1]["Tags"]:
+      value = datamap[1]["Tags"][paramm]
+      Key = {"Key": paramm, "Value": value}
+      Tags.append(Key)
+  print('Tags =', Tags)
+  try:
+      output=open('tags.json', 'w')
+  except FileNotFoundError:
+      print("can''t open destiantion file %s " % inputfile)
+      sys.exit(2)
+  json.dump(Tags, output)
+  output.flush()
+  output.close()
 
 
-inputfile = 'params-'+sys.argv[1]+'.yaml'
-print('inputfile is %s' % inputfile )
-stream = open(inputfile, 'r')
-datamap = yaml.safe_load(stream)
-print('json_obj =', datamap)
-output=open('params.json', 'w')
-json.dump(datamap, output)
-output.flush()
-output.close()
-stream.close()
+
+days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+
+for i,m in enumerate(days, start=3):
+    print(i,m)
+
+def main():
+    if len(sys.argv[1:]) < 1 :
+        sys.exit('should be described ENVIRONMENT')
+    envir = sys.argv[1]
+    if envir not in allowed_env:
+        print('wrong env - we process only', allowed_env)
+        sys.exit()
+
+    # print('the script has the name %s' % program_name)
+    print("the script will convert params-%s.yaml and tags-%s.yaml into params.json and tags.json" % (sys.argv[1], sys.argv[1]))
+    print('we will prepare params.json and tags.json for ENVIRONMENT %s' % envir)
+
+    print('')
+    print("Lets process params.yaml")
+
+    inputfile = 'params-'+sys.argv[1]+'.yaml'
+    process_yaml(inputfile, 'params.json')
+
+    print('')
+    print("Lets process tags.yaml")
+
+    inputfile = 'tags-'+sys.argv[1]+'.yaml'
+    #process_yaml(inputfile, 'tags.json')
 
 
-inputfile = 'tags-'+sys.argv[1]+'.yaml'
-print('inputfile is %s' % inputfile )
-stream = open(inputfile, 'r')
-datamap = yaml.safe_load(stream)
-print('json_obj =', datamap)
-output=open('tags.json', 'w')
-json.dump(datamap, output)
-output.flush()
-output.close()
-stream.close()
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
--- tf/main.tf
+++ tf/main.tf
@@ -5,22 +5,24 @@ terraform {
 provider "aws" {
     region = "${var.aws_region}"
 }
-
-resource "aws_vpc" "main" {
-    cidr_block = "${var.VPCBlock}"
-
-    tags = {
-        Name     = "${var.Environment}-vpc"
-    }
+module "vpc" {
+    source                              = "modules/vpc"
+    aws_region     	                    = "eu-west-3"
+    STACK                               = "AWS-NATGW"
+    Environment                         = "QA"
+    KeyName                             = "aws-test-oregon"
+    # NATGW_type                          = "t2.micro"
+    # NATGW_ami                           = "ami-0ebb3a801d5fb8b9b"
+    VPCBlock                            = "10.0.0.0/16"
+    PublicSubnetCIDR                    = "10.0.10.0/24"
+    PrivatSubnetCIDR                    = "10.0.11.0/24"
+    enable_dns_support                  = "true"
+    #Internet-GateWay
+    enable_internet_gateway             = "true"
+    #NAT
+    enable_nat_gateway                  = "false"
 }
-# Internet Gateway
-resource "aws_internet_gateway" "igw" {
-    vpc_id = "${aws_vpc.main.id}"
 
-    tags = {
-        Name     = "${var.Environment}-IGW"
-    }
-}
 # # EIP and NAT Gateway
 # resource "aws_eip" "nat_eip" {
 #   vpc      = true
@@ -55,118 +57,15 @@ resource "aws_internet_gateway" "igw" {
 #     type = "S"
 #   }
 # }
-terraform {
-    backend "s3" {
-        encrypt = true
-        bucket = "tfstate-aws-s3-bucket"
-        region = "eu-west-3"
-        key = "natgw/terraform.tfstate"
-    }
-}
-#==================================================== Public Subnet =========
-resource "aws_subnet" "public_subnet" {
-    vpc_id     = "${aws_vpc.main.id}"
-    cidr_block = "${var.PublicSubnetCIDR}"
+# terraform {
+#     backend "s3" {
+#         encrypt = true
+#         bucket = "tfstate-aws-s3-bucket"
+#         region = "eu-west-3"
+#         key = "natgw/terraform.tfstate"
+#     }
+# }
 
-    tags = {
-        Name = "${var.Environment}-public_subnet"
-    }
-}
-#====== Public RouteTables ========= Routes for Public Subnet RouteTables with IGW =========
-resource "aws_route_table" "public_route" {
-    vpc_id = "${aws_vpc.main.id}"
-    tags = {
-        Name = "${var.Environment}-PublicRouteTables"
-    }
-
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = "${aws_internet_gateway.igw.id}"
-    }
-}
-resource "aws_route_table_association" "public_route_assoc" {
-    subnet_id = "${aws_subnet.public_subnet.id}"
-    route_table_id = "${aws_route_table.public_route.id}"
-}
-#==================================================== Privat Subnet =========
-resource "aws_subnet" "privat_subnet" {
-    vpc_id     = "${aws_vpc.main.id}"
-    cidr_block = "${var.PrivatSubnetCIDR}"
-
-    tags = {
-        Name = "${var.Environment}-privat_subnet"
-    }
-}
-#====== Privat RouteTables ========= Routes for Privat Subnet RouteTables with NATGW =========
-resource "aws_default_route_table" "privat_route" {
-    default_route_table_id = "${aws_vpc.main.default_route_table_id}"
-    tags = {
-        Name = "${var.Environment}-PrivatRouteTables"
-    }
-
-    route {
-        cidr_block = "0.0.0.0/0"
-        # gateway_id = "${aws_internet_gateway.igw.id}"
-        instance_id = "${aws_instance.NATGWInstance.id}"
-    }
-    depends_on = ["aws_instance.NATGWInstance"]
-}
-resource "aws_route_table_association" "privat_route_assoc" {
-    subnet_id = "${aws_subnet.privat_subnet.id}"
-    route_table_id = "${aws_default_route_table.privat_route.id}"
-}
-#====== NAT GW SecurityGroup
-resource "aws_default_security_group" "NATGW_sg" {
-    # aws_default_security_group
-    # name = "natgw_sg"
-    tags = {
-        Name = "${var.Environment}-natgw_sg"
-    }
-    # description = "Connections for the nat instance"
-    vpc_id = "${aws_vpc.main.id}"
-    ingress {
-        from_port   = "0"
-        to_port     = "0"
-        protocol    = "-1"
-        cidr_blocks = ["${var.VPCBlock}"]
-    }
-    ingress {
-        from_port   = "22"
-        to_port     = "22"
-        protocol    = "TCP"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-}
-#====== NAT GW instance
-resource "aws_instance" "NATGWInstance" {
-    ami = "${var.NATGW_ami}"
-    instance_type = "${var.NATGW_type}"
-    associate_public_ip_address = "true"
-    key_name = "${var.KeyName}"
-
-    subnet_id = "${aws_subnet.public_subnet.id}"
-    vpc_security_group_ids = ["${aws_default_security_group.NATGW_sg.id}"]
-    source_dest_check = "false"
-    user_data = <<-EOF
-                #!/bin/bash -xe
-                #sed -i "s/net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/" /etc/sysctl.conf
-                echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
-                sysctl -p
-                echo "iptables -t nat -A POSTROUTING -s ${var.VPCBlock} -j MASQUERADE" >> /etc/rc.local
-                iptables -t nat -A POSTROUTING -s ${var.VPCBlock} -j MASQUERADE
-                EOF
-    tags = {
-        STACK = "${var.STACK}"
-        Name = "${var.Environment}-NATGW"
-        VM = "NATGW"
-    }
-}
 #====== BackEnd instance
 resource "aws_instance" "BackEndInstance" {
     ami = "${var.server_ami}"
@@ -179,51 +78,6 @@ resource "aws_instance" "BackEndInstance" {
         STACK = "${var.STACK}"
         Name = "${var.Environment}-BackEnd"
         VM = "BackEnd"
-    }
-}
-#====== Public SecurityGroup
-resource "aws_security_group" "public_sg" {
-    name = "public_sg"
-    tags = {
-        Name = "${var.Environment}-public_sg"
-    }
-    description = "Connections for the nat instance"
-    vpc_id = "${aws_vpc.main.id}"
-    ingress {
-        from_port   = "80"
-        to_port     = "80"
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    ingress {
-        from_port   = "8080"
-        to_port     = "8080"
-        protocol    = "TCP"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    ingress {
-        from_port   = "1050"
-        to_port     = "1052"
-        protocol    = "TCP"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    ingress {
-        from_port   = "12345"
-        to_port     = "12345"
-        protocol    = "TCP"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    ingress {
-        from_port   = "0"
-        to_port     = "0"
-        protocol    = "-1"
-        cidr_blocks = ["${var.VPCBlock}"]
-    }
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
     }
 }
 #====== Public Servers ====== Tomcat instance
